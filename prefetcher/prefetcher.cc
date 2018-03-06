00509b34fa056676f5b4f10c513587f241b3df9c
Your Name <jpitagc>
2018-03-06 12:50:13 +0100

    Add idea

--- prefetcher/prefetcher.cc
+++ prefetcher/prefetcher.cc
@@ -7,7 +7,10 @@
 #include "interface.hh"
 #include <stdlib.h>
 
-
+// IDEAS:
+//  * since the prefetching efficiency varies during the execution of a program, we propose to adapt the number of pref etched blocks according to a//    dynamic measure of prefetching effectiveness.  Can we access the stats whule running the excution? If the first test has already finished can //    we use it to predict the effectiveness of the next test. Using also the queue size (100).
+//
+//  
 void prefetch_init(void)
 {
     /* Called before any calls to prefetch_access. */
@@ -19,13 +22,13 @@ void prefetch_init(void)
 const size TABLE_SIZE = 20;
 
 struct Stride {
-	Addr pc;
+  Addr pc; // instead of addss use AccessStat 
 	Addr access[3];
 };
 
 Stride table[TABLE_SIZE];
 
-void refresh_or_insert(AccessStat stat) {
+void refresh_or_insert(AccessStat stat) { // checks for loops in the program with the pc 
 	for (int i = 0; i < TABLE_SIZE; ++i) {
 		if (table[i].pc == stat.pc) {
 			Stride temp = table[i];
@@ -40,7 +43,28 @@ void refresh_or_insert(AccessStat stat) {
 		table[j] = table[j + 1];
 	}
 	table[0].pc = stat.pc;
-	table[0].access[0] = stat.mem_addr;
+	// if (table[0].access[0]!= null/0){
+	//                if (table[0].access[1]!=0/null){
+	//                     int distance= table[0].access[0]-table[0].access[1];
+        //                    if(distance==table[0].access[1]-stat.mem_addre){
+	//                          if(!in_cache(stat.mem_address+ distance)){ // maybe a loop to prefetch the posible next x blocks.
+	//                               issue_prefetch(stat.mem_address+distance);
+	//                          }
+        //                     }
+	//
+	//                    for(int i=0; i>2; i++){
+        //                         table[0].access[i]=table[0].access[i+1];
+        //                    }
+        //                    table[0].access[0] = stat.mem_addr;
+	//
+	//                 }
+	//                 else {table[0].access[1]=stat.mem_address}
+        // }
+	// else {table[0].acess[0]= stat.mem_address}
+	//
+	//
+	//
+	
 }
 
 void prefetch_access(AccessStat stat)
