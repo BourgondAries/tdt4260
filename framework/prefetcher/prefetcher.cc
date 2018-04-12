8f704cf5485723ea4d4e868f1a7b426585ce72ae
Javier Pita <jpitagc@gmail.com>
2018-04-12 10:58:58 +0200

    Working Not Sigsev

--- framework/prefetcher/prefetcher.cc
+++ framework/prefetcher/prefetcher.cc
@@ -3,9 +3,9 @@
 
 // Delta Correlated Prediction s
 
-const int maxSize = 100;
-const int deltaBufferSize = 20;
-const int posiblePrefetchesSize = 10;
+const int maxSize = 200;
+const int deltaBufferSize = 50;
+
 
 struct entry {
 	Addr deltaBuffer[deltaBufferSize]; // buffer of delata stored
@@ -28,25 +28,46 @@ void loop_prefetch(AccessStat stat) {
 	// if(newDelta == 0) return;
 	t.s[0].lastaddr = stat.mem_addr; // store the new last addres
 	if (t.s[0].deltaBufferPosition > 3) {
-		Addr lastDelta = t.s[0].deltaBuffer[t.s[0].deltaBufferPosition-1 % deltaBufferSize]; // create a pair of deltas
-		for (int i = 0; i <= deltaBufferSize; ++i) { // pattern matching
-			if (t.s[0].deltaBuffer[i] == lastDelta && t.s[0].deltaBuffer[i+1] == newDelta){
+
+		Addr lastDelta = t.s[0].deltaBuffer[t.s[0].deltaBufferPosition-1]; // create a pair of deltas
+    int patternMatchingPointer = t.s[0].deltaBufferPosition;
+		for (int i = 0; i < deltaBufferSize-1 && patternMatchingPointer!= t.s[0].deltaBufferPosition-1; ++i) { // pattern matching
+
+			if (t.s[0].deltaBuffer[patternMatchingPointer] == lastDelta && t.s[0].deltaBuffer[patternMatchingPointer+1] == newDelta){
+
+
+
+
+        int posiblePrefetchesSize = 0;
+
+        if (patternMatchingPointer > t.s[0].deltaBufferPosition){
+          posiblePrefetchesSize = deltaBufferSize-1 - patternMatchingPointer + t.s[0].deltaBufferPosition -2;
+
+        }else{
+           posiblePrefetchesSize = t.s[0].deltaBufferPosition -2 - patternMatchingPointer;
+           if(posiblePrefetchesSize<0) posiblePrefetchesSize =0;
+        }
+
+
 				Addr posiblePrefetches[posiblePrefetchesSize] ;
-				int posPattern = i+2;
+				int posPattern = patternMatchingPointer + 2;
 				Addr lastPosiblePrefetch = stat.mem_addr;
 				int alreadyPrefetched = 0;
+
 				for (int j = 0 ; j < posiblePrefetchesSize; ++j) {
-					posiblePrefetches[j] = lastPosiblePrefetch + t.s[0].deltaBuffer[(posPattern+j)%deltaBufferSize];
+
+					posiblePrefetches[j] = lastPosiblePrefetch + t.s[0].deltaBuffer[posPattern];
 					lastPosiblePrefetch = posiblePrefetches[j]; // update posible prefetch
 					if(lastPosiblePrefetch == t.s[0].lastprefetch) {
 						alreadyPrefetched = j; // check if the posible prefetches are already prefetch
 					}
+          posPattern = (posPattern+1)%deltaBufferSize;
 					// DPRINTF(HWPrefetch, "5: \n");
 				}
 				// DPRINTF(HWPrefetch, "int J: %d int posiblePrefetches %d \n" j, posiblePrefetches);
 				for (int i = alreadyPrefetched; i< posiblePrefetchesSize ; i++){
 					DPRINTF(HWPrefetch, "5: \n");
-					Addr toprefetch= posiblePrefetches[j];
+					Addr toprefetch= posiblePrefetches[i];
 					if (get_prefetch_bit(toprefetch)== false && toprefetch < MAX_PHYS_MEM_ADDR){
 						issue_prefetch(toprefetch);
 						set_prefetch_bit(toprefetch);
@@ -55,10 +76,12 @@ void loop_prefetch(AccessStat stat) {
 				}
 				break;
 			}
+
+      patternMatchingPointer= (patternMatchingPointer+1)% deltaBufferSize;
 		}
 	}
-	t.s[0].deltaBuffer[t.s[0].deltaBufferPosition % deltaBufferSize]= newDelta;
-	t.s[0].deltaBufferPosition++;
+	t.s[0].deltaBuffer[t.s[0].deltaBufferPosition]= newDelta;
+	t.s[0].deltaBufferPosition= (t.s[0].deltaBufferPosition+1)% deltaBufferSize;
 }
 
 void refresh_or_insert(AccessStat stat) {
@@ -86,6 +109,12 @@ void refresh_or_insert(AccessStat stat) {
 
 	t.s[0].key= stat.pc;
 	t.s[0].lastaddr = stat.mem_addr;
+
+  for(int i = 0; i < deltaBufferSize; i++){
+    t.s[0].deltaBuffer[i]= 0;
+  }
+  t.s[0].lastprefetch=0;
+  t.s[0].deltaBufferPosition=0;
 
 	if(t.elements < maxSize) {
 		t.elements++;
